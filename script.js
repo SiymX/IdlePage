3e9ed4e56f1cd580f17a9ceb8d9ae86088e3e154
SiymX <khsayeem17@gmail.com>
2023-05-14 13:11:42 -0400

    third commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let timer = document.getElementById("timer");
 
 const channel = new BroadcastChannel('timerChannel');
+const KEEP_ALIVE_INTERVAL = 2000; 
+const KEEP_ALIVE_THRESHOLD = 5000; 
 
 let start;
 let elapsed;
@@ -10,7 +12,8 @@ channel.onmessage = (event) => {
     elapsed = event.data.elapsed;
 };
 
-if (localStorage.getItem("start")) {
+if (localStorage.getItem("start") &&
+    Date.now() - Number(localStorage.getItem("lastSeen")) < KEEP_ALIVE_THRESHOLD) {
     start = Number(localStorage.getItem("start"));
     elapsed = Number(localStorage.getItem("elapsed"));
 } else {
@@ -21,6 +24,7 @@ if (localStorage.getItem("start")) {
 let interval = setInterval(() => {
     elapsed = Date.now() - start;
     localStorage.setItem("elapsed", elapsed);
+    localStorage.setItem("lastSeen", Date.now());
 
     channel.postMessage({start: start, elapsed: elapsed});
 
@@ -38,3 +42,7 @@ let interval = setInterval(() => {
                        <span class="timePart"><span class="time">${seconds.toString().padStart(2, '0')}</span><span class="label">s</span></span>
                        <span class="timePart"><span class="time ms">${milliseconds.toString().padStart(3, '0')}</span><span class="label ms">ms</span></span>`;
 }, 10);
+
+setInterval(() => {
+    localStorage.setItem("lastSeen", Date.now());
+}, KEEP_ALIVE_INTERVAL);
